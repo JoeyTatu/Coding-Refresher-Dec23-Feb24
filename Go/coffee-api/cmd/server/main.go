7c9c892266b25bc731a3c1f65b54a6819967e822
Joey Tatú <joeyctatu@gmail.com>
2023-12-08 12:41:25 +0000

    Edited

--- Go/coffee-api/cmd/server/main.go
+++ Go/coffee-api/cmd/server/main.go
@@ -1,5 +1,51 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/JoeyTatu/coffee-api/db"
+)
+
+type Config struct {
+	Port string
+}
+
+type Application struct {
+	Config Config
+}
+
+// Global port variable for both Main and Serve
+var port = os.Getenv("PORT") // 8080
+
+func (app *Application) Serve() error {
+	srv := &http.Server{
+		Addr: fmt.Sprintf(":%s", port),
+	}
+
+	return srv.ListenAndServe()
+}
+
 func main() {
-	println("This works")
+	var cfg Config
+	cfg.Port = port
+
+	dsn := os.Getenv("DSN")
+	dbConn, err := db.ConnectPostgres(dsn)
+	if err != nil {
+		log.Fatal("Cannot connect to database!", err)
+	}
+
+	defer dbConn.DB.Close()
+
+	app := &Application{
+		Config: cfg,
+	}
+
+	err = app.Serve()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
