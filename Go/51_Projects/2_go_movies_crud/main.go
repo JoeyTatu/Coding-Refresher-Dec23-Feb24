051fedff99d15028572bff62809e035fa79c8c1b
Joey Tatú <joeyctatu@gmail.com>
2023-12-13 20:42:33 +0000

    Moved go.mod to main Go folder

--- Go/51_Projects/2_go_movies_crud/main.go
+++ Go/51_Projects/2_go_movies_crud/main.go
@@ -1,1 +1,146 @@
 package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/google/uuid"
+	"github.com/gorilla/mux"
+)
+
+type Movie struct {
+	ID       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct {
+	First_name string `json:"firstName"`
+	Last_name  string `json:"lastName"`
+}
+
+var movies []Movie
+
+func generateUniqueID() string {
+	id := uuid.New()
+	return id.String()
+}
+
+func setJSONContentType(w http.ResponseWriter) {
+	w.Header().Set("Content-Type", "application/json")
+}
+
+func getAllMovies(w http.ResponseWriter, r *http.Request) {
+	setJSONContentType(w)
+	json.NewEncoder(w).Encode(movies)
+}
+
+func getMovieById(w http.ResponseWriter, r *http.Request) {
+	setJSONContentType(w)
+	params := mux.Vars(r)
+	for _, mov := range movies {
+
+		if mov.ID == params["id"] {
+			json.NewEncoder(w).Encode(mov)
+			return
+		}
+	}
+}
+
+func createMovie(w http.ResponseWriter, r *http.Request) {
+	setJSONContentType(w)
+
+	var newMovie Movie
+	err := json.NewDecoder(r.Body).Decode(&newMovie)
+	if err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	newMovie.ID = generateUniqueID()
+
+	movies = append(movies, newMovie)
+	json.NewEncoder(w).Encode(newMovie)
+}
+
+func updateMovie(w http.ResponseWriter, r *http.Request) {
+	setJSONContentType(w)
+
+	params := mux.Vars(r)
+	movieID := params["id"]
+
+	var updatedMovie Movie
+	err := json.NewDecoder(r.Body).Decode(&updatedMovie)
+	if err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	// Check if the movie ID is "1" or "2" and skip the update
+	if movieID == "1" || movieID == "2" {
+		http.Error(w, "Cannot update movie with ID 1 or 2", http.StatusForbidden)
+		return
+	}
+
+	for index, mov := range movies {
+		if mov.ID == movieID {
+			movies[index] = updatedMovie
+			json.NewEncoder(w).Encode(updatedMovie)
+			return
+		}
+	}
+
+	http.Error(w, "Movie not found", http.StatusNotFound)
+}
+
+func deleteMovie(w http.ResponseWriter, r *http.Request) {
+	setJSONContentType(w)
+	params := mux.Vars(r)
+	movieID := params["id"]
+
+	// Check if the movie ID is "1" or "2" and skip the deletion
+	if movieID == "1" || movieID == "2" {
+		http.Error(w, "Cannot delete movie with ID 1 or 2", http.StatusForbidden)
+		return
+	}
+
+	for index, mov := range movies {
+		if mov.ID == movieID {
+			movies = append(movies[:index], movies[index+1:]...)
+			break
+		}
+	}
+	getAllMovies(w, r)
+}
+
+func main() {
+	r := mux.NewRouter()
+
+	movies = append(movies, Movie{
+		ID:    "1",
+		Isbn:  "TEST_DATA1",
+		Title: "Whispers in the Shadows",
+		Director: &Director{
+			First_name: "Olivia",
+			Last_name:  "Bennett"}})
+
+	movies = append(movies, Movie{
+		ID:    "2",
+		Isbn:  "TEST_DATA2",
+		Title: "Echoes of Eternity",
+		Director: &Director{
+			First_name: "Samantha",
+			Last_name:  "Harper"}})
+
+	r.HandleFunc("/movies", getAllMovies).Methods("GET")
+	r.HandleFunc("/movies/movie/{id}", getMovieById).Methods("GET")
+	r.HandleFunc("/movies/movie", createMovie).Methods("POST")
+	r.HandleFunc("/movies/movie/{id}", updateMovie).Methods("PUT", "PATCH")
+	r.HandleFunc("/movies/movie/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Printf("Starting server at port 8000")
+	log.Fatal((http.ListenAndServe(":8000", r)))
+}
--- Go/go.mod
+++ Go/go.mod
@@ -1,39 +1,37 @@
-// Made this a bak file as it was interfering with other projects.
-
-module github.com/JoeyTatu/coffee-api
+module Go_Folder
 
 go 1.21.4
 
 require (
-	github.com/go-chi/chi/v5 v5.0.8
-	github.com/go-chi/cors v1.2.1
-	github.com/jackc/pgconn v1.14.0
-	github.com/jackc/pgx/v4 v4.18.1
-	github.com/lib/pq v1.10.7
-)
-
-require (
 	github.com/andybalholm/brotli v1.0.6 // indirect
+	github.com/go-chi/chi/v5 v5.0.10 // indirect
+	github.com/go-chi/cors v1.2.1 // indirect
 	github.com/gofiber/fiber/v2 v2.51.0 // indirect
-	github.com/google/uuid v1.4.0 // indirect
+	github.com/google/uuid v1.5.0 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.14.1 // indirect
 	github.com/jackc/pgio v1.0.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgproto3/v2 v2.3.2 // indirect
-	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgservicefile v0.0.0-20231201235250-de7065d80cb9 // indirect
 	github.com/jackc/pgtype v1.14.0 // indirect
+	github.com/jackc/pgx/v4 v4.18.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/klauspost/compress v1.17.4 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/mattn/go-sqlite3 v1.14.8 // indirect
 	github.com/rivo/uniseg v0.4.4 // indirect
+	github.com/sixfwa/fiber-gorm v0.0.0-20211012211537-1f193f512c01 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.51.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
-	golang.org/x/crypto v0.14.0 // indirect
+	golang.org/x/crypto v0.16.0 // indirect
 	golang.org/x/sys v0.15.0 // indirect
-	golang.org/x/text v0.13.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	gorm.io/driver/sqlite v1.1.6 // indirect
 	gorm.io/gorm v1.25.5 // indirect
 )
